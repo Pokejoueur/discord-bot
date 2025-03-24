24f268639c47eb9bade61551454f0c5e00df98cd
Pokejoueur <106403938+Pokejoueur@users.noreply.github.com>
2025-03-24 22:53:25 +0100

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,1 +1,39 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const Ticket = require('../models/tickets');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ticket')
+        .setDescription('Open a support ticket'),
+    
+    async execute(interaction) {
+        const { guild, user } = interaction;
+
+        // Check if user already has a ticket
+        const existingTicket = await Ticket.findOne({ userId: user.id });
+        if (existingTicket) {
+            return interaction.reply({ content: "You already have an open ticket!", ephemeral: true });
+        }
+
+        // Create a ticket channel
+        const ticketChannel = await guild.channels.create({
+            name: `ticket-${user.username}`,
+            type: 0, // Text channel
+            permissionOverwrites: [
+                {
+                    id: guild.roles.everyone.id,
+                    deny: [PermissionFlagsBits.ViewChannel],
+                },
+                {
+                    id: user.id,
+                    allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages],
+                }
+            ]
+        });
+
+        // Save ticket to database
+        await Ticket.create({ userId: user.id, channelId: ticketChannel.id });
+
+        await interaction.reply({ content: `🎫 Your ticket has been created: ${ticketChannel}`, ephemeral: true });
+    }
+};
