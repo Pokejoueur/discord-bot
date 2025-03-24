ff8275d80f68b6d7b0a39edb624191ac14a829dc
Pokejoueur <106403938+Pokejoueur@users.noreply.github.com>
2025-03-24 22:40:36 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,12 @@
+const generateWelcomeImage = require('../utils/generateWelcomeImage');
 
+module.exports = {
+    name: 'guildMemberAdd',
+    async execute(member) {
+        const welcomeChannel = member.guild.channels.cache.find(ch => ch.name === 'welcome'); // Change channel name if needed
+        if (!welcomeChannel) return;
+
+        const attachment = await generateWelcomeImage(member);
+        welcomeChannel.send({ content: `🎉 Welcome <@${member.user.id}> to **${member.guild.name}**!`, files: [attachment] });
+    }
+};
