6fd4fd8ef1effd395b713818d9df859d552b943f
Pokejoueur <106403938+Pokejoueur@users.noreply.github.com>
2025-03-24 22:28:30 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,22 @@
+const XP = require('../models/xp');
 
+module.exports = {
+    name: 'messageCreate',
+    async execute(message) {
+        if (message.author.bot) return;
+
+        const user = await XP.findOne({ userId: message.author.id }) || new XP({ userId: message.author.id });
+
+        const xpGain = Math.floor(Math.random() * 10) + 5;
+        user.xp += xpGain;
+
+        const nextLevelXP = user.level * 100;
+        if (user.xp >= nextLevelXP) {
+            user.xp = 0;
+            user.level += 1;
+            message.channel.send(`🎉 **${message.author.username}** leveled up to **Level ${user.level}**!`);
+        }
+
+        await user.save();
+    }
+};
