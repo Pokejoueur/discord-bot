b8a53571967391739aa2ef68ddf5b9772ce1c72c
Pokejoueur <106403938+Pokejoueur@users.noreply.github.com>
2025-03-24 22:23:05 +0100

    Update work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Economy = require('../../models/economy');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('work')
+        .setDescription('Work to earn coins'),
+    async execute(interaction) {
+        const earnings = Math.floor(Math.random() * 200) + 50;
+        const user = await Economy.findOne({ userId: interaction.user.id }) || new Economy({ userId: interaction.user.id });
+
+        user.balance += earnings;
+        await user.save();
+
+        await interaction.reply(`💼 **${interaction.user.username}**, you worked and earned **${earnings} coins**!`);
+    }
+};
