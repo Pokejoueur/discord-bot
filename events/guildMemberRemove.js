387016b8de04a89370ee1c5d60010255d448e9bb
Pokejoueur <106403938+Pokejoueur@users.noreply.github.com>
2025-03-24 22:41:03 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,1 +1,9 @@
+module.exports = {
+    name: 'guildMemberRemove',
+    async execute(member) {
+        const goodbyeChannel = member.guild.channels.cache.find(ch => ch.name === 'goodbye'); // Change channel name if needed
+        if (!goodbyeChannel) return;
 
+        goodbyeChannel.send(`😢 **${member.user.username}** has left the server. We'll miss you!`);
+    }
+};
